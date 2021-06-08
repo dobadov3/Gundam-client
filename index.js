33dbd4df8c0f0b5ab5e6dbfac06af2b69303951f
dobadov3 <70206718+dobadov3@users.noreply.github.com>
2021-06-08 13:07:39 +0700

    Done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
